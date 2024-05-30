ca02e96a3fa9162db3cd2324dee9a71b4ed05fef
TheBigStonk <maxfrancis212@gmail.com>
2024-05-30 22:52:25 +1200

    Post out initial POC

---
+++ Controllers/EmailSearchController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SolrNet;
+using SolrNet.Commands.Parameters;
+using System.Collections.Generic;
+
+namespace api.enronsearch.com.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class EmailSearchController : ControllerBase
+    {
+        private readonly ISolrOperations<Email> _solr;
+
+        public EmailSearchController(ISolrOperations<Email> solr)
+        {
+            _solr = solr;
+        }
+
+        [HttpGet]
+        public IEnumerable<Email> Get([FromQuery] string searchQuery)
+        {
+            // Limit the query to return only 10 documents and also return different results each time. Although I am limiting by 10, I also want to avoid making some emails hidden if the search term is generic (i.e., fraud)
+            var randomSeed = new System.Random().Next();
+            var queryOptions = new QueryOptions
+            {
+                Rows = 10,
+                OrderBy = new[] { new SortOrder("random_" + randomSeed.ToString()) }
+            };
+
+            var results = _solr.Query(new SolrQueryByField("Content", searchQuery), queryOptions);
+            return results;
+        }
+    }
+}
---
+++ Email.cs
@@ -0,0 +1,28 @@
+﻿using SolrNet.Attributes;
+
+namespace api.enronsearch.com
+{
+    public class Email
+    {
+        [SolrUniqueKey("id")]
+        public string Id { get; set; }
+
+        [SolrField("Date")]
+        public List<string> Date { get; set; }
+
+        [SolrField("From")]
+        public List<string> From { get; set; }
+
+        [SolrField("To")]
+        public List<string> To { get; set; }
+
+        [SolrField("Cc")]
+        public List<string> Cc { get; set; }
+
+        [SolrField("Subject")]
+        public List<string> Subject { get; set; }
+
+        [SolrField("Content")]
+        public List<string> Content { get; set; }
+    }
+}
